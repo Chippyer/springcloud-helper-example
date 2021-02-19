d6ddb7c5956b1acc99a2ab51b4b7d8667f461fe3
Chippyer <450799968@qq.com>
2021-02-20 03:07:26 +0800

    新增web全局日志组件配置

--- user-service/src/main/java/com/chippy/example/mapper/UserMapper.java
+++ user-service/src/main/java/com/chippy/example/mapper/UserMapper.java
@@ -1,7 +1,14 @@
 package com.chippy.example.mapper;
 
 import com.chippy.example.entity.User;
+import com.ejoy.tkmapper.A;
 import tk.mybatis.mapper.common.Mapper;
 
-public interface UserMapper extends Mapper<User> {
+public interface UserMapper extends Mapper<User>, A {
+
+    @Override
+    default Class getC() {
+        return User.class;
+    }
+
 }
